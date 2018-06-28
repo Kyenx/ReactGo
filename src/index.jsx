2724ebf0315146b9d10e6bca1058b891d1d263be
Jkyenx <jovin.kyenx@gmail.com>
2018-06-28 10:15:09 -0400

    Init commit

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {hot} from "react-hot-loader";
+import { render } from 'react-dom';
+import App from './components/App';
+
+
+render(<App />, document.getElementById('main'));
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var webpack = require('webpack');
+var path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+var BUILD_DIR = path.resolve(__dirname, 'dist');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./public/index.html",
+  filename: "./index.html"
+});
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    rules : [
+      {
+        test : /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+        loader : 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  plugins: [ htmlPlugin ]
+};
+
+module.exports = config;
